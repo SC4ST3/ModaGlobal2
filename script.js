d3aa616ca4befbe6e9cf112927dd4c92d5c2a420
Sergio Castellon <sergio.caste.87@gmail.com>
2025-12-08 00:36:09 +0100

    Eliminat script.js a la branca conflicte-eliminacio

--- script.js
+++
@@ -1,9 +0,0 @@
-function afegirContacte(nom) {
-    if (!nom || nom.trim() === "") {
-        console.log("No es pot afegir un contacte en blanc");
-        alert("El nom del contacte no pot estar en blanc.");
-        return;
-    }
-
-    console.log("Contacte afegit: " + nom);
-}
